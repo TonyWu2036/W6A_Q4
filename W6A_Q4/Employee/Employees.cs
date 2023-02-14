1a9cfe69849476ec38cc119f47acffe14a9d191b
Xincheng Wu <wuxinc@sheridancollege.ca>
2023-02-14 10:07:42 -0500

    Salaried

---
+++ W6A_Q4/Employee/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6A_Q4.Employee
+{
+    public class Employees
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _sinNumber;
+
+        public virtual  double Calculatesalary()
+        {
+        return 0;
+        }
+    }
+}
